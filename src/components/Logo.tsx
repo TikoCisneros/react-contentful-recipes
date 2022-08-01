9a89f72838acd822fb5433b7e3cc0b67d2d5810c
Luis Cisneros <ext_lucisneros@falabella.cl>
2022-08-01 14:01:39 -0500

    style: minimal improvements in Logo styles

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,10 +2,10 @@ import styles from "./Logo.module.css";
 
 const Logo = () => {
   return (
-    <section className="flex mb-8 items-end">
-      <img src="./img/logo-cook.png" alt="logo-dr-cook" className="w-36" />
+    <section className="flex mb-5 items-end justify-center lg:justify-start">
+      <img src="./img/logo-cook.png" alt="logo-dr-cook" className="w-[7.5rem]" />
       <span
-        className={`text-[3.2rem] italic font-semibold text-red-800 drop-shadow-lg shadow-black ml-2 ${styles.logo__text}`}
+        className={`text-[3.6rem] italic font-semibold text-red-800 drop-shadow-lg shadow-black ml-2 ${styles.logo__text}`}
       >
         Dr. Cook
       </span>
